a630950de35fec25974f7e9b0043991e37adbdc9
Yashita Sharma <yashitasharma9tha@gmail.com>
2026-09-08 00:58:01 +0530

    Time: 40 ms (59.26%) | Memory: 12.5 MB (47.15%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        ans = []
+
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i + n])
+
+        return ans
+        
