0357dc173ebc58acd3347206506ce6d522eedaf6
Yashita Sharma <yashitasharma9tha@gmail.com>
2026-09-17 00:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (80.33%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        if left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+
+        for i in range(left - 1):
+            prev = prev.next
+
+        curr = prev.next
+
+        for i in range(right - left):
+            temp = curr.next
+            curr.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
+
+        
+        
