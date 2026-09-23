a0bb552db52867eeebef1255ba40856de00042ea
Yashita Sharma <yashitasharma9tha@gmail.com>
2026-09-23 16:02:10 +0530

    Time: 218 ms (79.34%) | Memory: 20.8 MB (47.17%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        buy1 = float('-inf')
+        sell1 = 0
+        buy2 = float('-inf')
+        sell2 = 0
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1+price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+        return sell2
+        
